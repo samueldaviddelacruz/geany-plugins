19c0d59fdb56df852d9a490c737db3acfebb6820
Samuel <alphaelena@gmail.com>
2020-05-28 01:45:35 -0400

    Added print errors to compiler window functionality

--- go-fmt-plugin.c
+++ go-fmt-plugin.c
@@ -17,8 +17,9 @@ const char GO_FILE_EXTENSION[10] = "go";
 static void document_before_save(GObject *obj, GeanyDocument *doc, gpointer user_data)
 {
 	printf("Example: %s is about to be saved\n", DOC_FILENAME(doc));
-
-	int is_go_file_type = strcmp(doc->file_type->extension, GO_FILE_EXTENSION) == 0;
+	
+	
+	int is_go_file_type = strcmp(doc->file_type->extension, GO_FILE_EXTENSION) == false;
 	if (doc == NULL)
 		doc = document_get_current();
 
@@ -39,8 +40,10 @@ static void document_before_save(GObject *obj, GeanyDocument *doc, gpointer user
 		bool was_changed;
 
 		was_changed = doc->changed;
-		sci = doc->editor->sci;
 
+
+		sci = doc->editor->sci;
+		
 		//format entire doc on save
 		offset = 0;
 		length = sci_get_length(sci);
@@ -52,29 +55,61 @@ static void document_before_save(GObject *obj, GeanyDocument *doc, gpointer user
 
 		formatted = g_string_sized_new(sci_len);
 
+
+		//create temp file to hold the formatted text for gofmt to read from
+		char tempFileNameBuff[32] = "/tmp/myTmpFile-XXXXXX";
+		int tempFileDescriptor = mkstemp(tempFileNameBuff);
+
+		 if(tempFileDescriptor<1)
+		{
+			printf("\n Creation of temp file failed with error [%s]\n","");
+			return;
+		}
+		
+		char *doc_content = sci_get_contents(sci,sci_len);
+		close(tempFileDescriptor);
+
+		//open file and write current document content to it
+		FILE *tempFilePointer;
+		tempFilePointer = fopen(tempFileNameBuff, "w+");
+		fputs(doc_content, tempFilePointer);
+		fclose(tempFilePointer);
+
+		//create command string to gofmt the temp file
 		char cmd_buffer[256];
-
-		sprintf(cmd_buffer, "%s %s", "gofmt", DOC_FILENAME(doc));
+		//"%s %s 2>&1",
+		sprintf(cmd_buffer, "%s %s", "gofmt", tempFileNameBuff );
 		FILE *fp;
+
 		char path[4096];
-
-		/* Open the command for reading. */
-		fp = popen(cmd_buffer, "r");
-		if (fp == NULL)
-		{
-			g_warning("Failed to run command");
-			return;		
+	
+		GError **error;
+		GString *sdoutput = g_string_new(NULL);
+		GString *stderroutput = g_string_new(NULL);
+		bool success = spawn_sync(NULL,cmd_buffer,NULL,NULL,NULL,sdoutput,stderroutput,NULL,error);
+		
+		if(success == true){
+			if(stderroutput->len > 0){		
+				gchar **errors = g_strsplit(stderroutput->str,"\n",-1);
+				int i = 0;
+				while(errors[i] != NULL) {
+					GString *error = g_string_new(errors[i]);
+					utils_string_replace_all(error, tempFileNameBuff, "Error at line"); 
+					msgwin_compiler_add(COLOR_RED,"%s",error->str);
+					i++;
+				}				
+				return;
+			}else{
+				g_string_append(formatted, sdoutput->str);
+			}					
 		}
-
-		/* Read the output a line at a time - output it. */
-		while (fgets(path, sizeof(path), fp) != NULL)
-		{
-			g_string_append(formatted, path);
-			printf("%s", path);
-		}
-
+		
+		
+		changed = (formatted->len != sci_len) ||
+              (g_strcmp0(formatted->str, sci_buf) != 0);
+		
 		old_first_line = scintilla_send_message(sci, SCI_GETFIRSTVISIBLELINE, 0, 0);
-		printf("%s", formatted->str);
+		
 
 		// Replace document text and move cursor to new position
 		scintilla_send_message(sci, SCI_BEGINUNDOACTION, 0, 0);
@@ -86,12 +121,16 @@ static void document_before_save(GObject *obj, GeanyDocument *doc, gpointer user
 		line_delta = new_first_line - old_first_line;
 		scintilla_send_message(sci, SCI_LINESCROLL, 0, -line_delta);
 		scintilla_send_message(sci, SCI_ENDUNDOACTION, 0, 0);
-
+			
 		document_set_text_changed(doc, (was_changed || changed));
 
 		/* close */
 		g_string_free(formatted, true);
-		pclose(fp);
+
+		
+		// remove temp files temporary file is deleted
+		remove(tempFileNameBuff);
+		
 	}
 }
 static gboolean hello_init(GeanyPlugin *plugin, gpointer pdata)
